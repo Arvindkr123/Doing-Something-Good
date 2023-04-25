fd83cdafba5582912e655bd64a0d704f894f8861
Arvind <thakurarvindkr10@gmail.com>
2023-04-25 09:39:13 +0530

    props types

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React from 'react'
 import Hello from './components/Hello';
 
 const App = () => {
+    const friends = ['Arvind K', 'Shayam Bhatnagar', 'Saurav K', 'Manmohan K']
     return (
         <div>
-            <Hello name={'Arvind K'} age={22}>
+            <Hello name={'Arvind K'} age={22} isMarried={false} friends ={friends}>
                 <p>I'm children of the Hello </p>
             </Hello>
         </div>
--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,13 +1,28 @@
 import React from 'react'
+import PropTypes, { element } from 'prop-types'
 
-const Hello = ({name, age, children}) => {
-  return (
-      <div>
-          <h1>My name is {name}</h1>
-          <h2>My age is {age}</h2>
-          <p>{children}</p>
-    </div>
-  )
+const Hello = ({ name, age, children, isMarried, friends }) => {
+    return (
+        <div>
+            <h1>My name is {name}</h1>
+            <h2>My age is {age}</h2>
+            <h2>Is married {isMarried.toString()}</h2>
+            <p>{children}</p>
+            <p>My friends listed below </p>
+            {friends.map((element, index) => {
+                return <ul key={index} >
+                    <li >{element}</li> 
+                </ul>
+            })}
+        </div>
+    )
 }
 
-export default Hello
+Hello.propTypes = {
+    name: PropTypes.string,
+    age: PropTypes.number,
+    isMarried: PropTypes.bool,
+    friends : PropTypes.array
+}
+
+export default Hello;
