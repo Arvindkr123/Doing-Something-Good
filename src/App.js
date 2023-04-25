ec03da0f4d06320f47cb223b0d9a7669853b5c01
Arvind <thakurarvindkr10@gmail.com>
2023-04-25 09:57:42 +0530

    default export and named export

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react'
-import Hello from './components/Hello';
+import './App.css'
+import Hello, { Greeting } from './components/Hello';
 
 const App = () => {
-    const friends = ['Arvind K', 'Shayam Bhatnagar', 'Saurav K', 'Manmohan K']
     return (
-        <div>
-            <Hello name={'Arvind K'} age={22} isMarried={false} friends ={friends}>
-                <p>I'm children of the Hello </p>
-            </Hello>
-        </div>
+        <>
+            <Greeting />
+            <Hello />
+        </>
     )
 }
 
--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,28 +1,18 @@
 import React from 'react'
-import PropTypes, { element } from 'prop-types'
 
-const Hello = ({ name, age, children, isMarried, friends }) => {
+const Hello = () => {
     return (
         <div>
-            <h1>My name is {name}</h1>
-            <h2>My age is {age}</h2>
-            <h2>Is married {isMarried.toString()}</h2>
-            <p>{children}</p>
-            <p>My friends listed below </p>
-            {friends.map((element, index) => {
-                return <ul key={index} >
-                    <li >{element}</li> 
-                </ul>
-            })}
+            <h1>welcome to the Export default and Named Export</h1>
+            <p>in  Named export we can't change the name but in Default export</p>
         </div>
     )
 }
 
-Hello.propTypes = {
-    name: PropTypes.string,
-    age: PropTypes.number,
-    isMarried: PropTypes.bool,
-    friends : PropTypes.array
+export const Greeting = () => {
+    return (
+        <div>WELCOME ARVIND K</div>
+    )
 }
 
 export default Hello;
