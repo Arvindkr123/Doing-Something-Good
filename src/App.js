ea9b0806566268266c5b3cb90145f53fa51bcec9
Arvind <thakurarvindkr10@gmail.com>
2023-04-27 19:40:19 +0530

    added useState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import './App.css'
 import Hello from './components/Hello';
+import UseStateWithArray from './components/UseStateWithArray';
 
 const App = () => {
     return (
         <>
-            <Hello/>
+            <UseStateWithArray/>
         </>
     )
 }
